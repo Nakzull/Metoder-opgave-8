531be70ce84b074dd8f1f59b3ddf6e4267579534
Nakzull <andr25f4@zbc.dk>
2021-08-12 12:09:41 +0200

    Add project files.

---
+++ Metoder opgave 8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metoder_opgave_8
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            List<int> listeB = new List<int>();
+
+            for (int i = 2; i <= 20; i += 2)
+            {
+                listeB.Add(i);
+            }
+            listeB = RemoveNumbers(listeB);
+
+            int listeCount = listeB.Count;
+
+            listeB.Insert(2, 17);
+
+            List<int> listeA = new List<int>();
+
+            listeA = listeB;
+
+            listeA.Reverse();
+
+        }
+        public static List<int> RemoveNumbers(List<int> a)
+        {
+            for (int i = 0; i < a.Count; i++)
+                if (i % 3 == 0)
+                    a.RemoveAt(i);
+            return a;
+        }
+}
+
+}
